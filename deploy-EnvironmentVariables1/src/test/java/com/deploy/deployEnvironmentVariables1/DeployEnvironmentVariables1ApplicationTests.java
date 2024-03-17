58a0f336cbee7f4432fdae1416b190447244066d
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-03-17 22:23:11 +0100

    Add files via upload

---
+++ deploy-EnvironmentVariables1/src/test/java/com/deploy/deployEnvironmentVariables1/DeployEnvironmentVariables1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.deployEnvironmentVariables1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEnvironmentVariables1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
