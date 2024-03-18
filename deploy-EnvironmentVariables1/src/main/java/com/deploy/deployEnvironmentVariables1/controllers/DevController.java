2c35dde622df6549dd660683ab1dd43f3fc74e06
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-03-18 12:35:17 +0100

    esercizio caricato

---
+++ deploy-EnvironmentVariables1/src/main/java/com/deploy/deployEnvironmentVariables1/controllers/DevController.java
@@ -0,0 +1,27 @@
+package com.deploy.deployEnvironmentVariables1.controllers;
+
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class DevController {
+
+    @Value("${authCode}")
+    private String authCode;
+
+    @Value("${devName}")
+    private String devName;
+
+    @GetMapping
+    public ResponseEntity<String> getHello() {
+        String hello = "Hello " + devName + ". Auth Code = " + authCode;
+        return ResponseEntity.ok(hello);
+    }
+
+}
